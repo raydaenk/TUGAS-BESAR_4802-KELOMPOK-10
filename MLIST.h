29cc5b43f544aa8cb870ead980cbd7f4da11a1d9
raydaenk <m.rayyadaengraja@gmail.com>
2025-12-20 10:16:42 +0700

    Update MLIST.h

--- MLIST.h
+++ MLIST.h
@@ -1,7 +1,7 @@
+
 #ifndef MLIST_H_INCLUDED
 #define MLIST_H_INCLUDED
 
-#include <iostream>
 #include <string>
 using namespace std;
 
@@ -9,12 +9,14 @@ struct Film;
 struct Actor;
 struct Relation;
 
+/* ================= STRUCT ================= */
+
 struct Film {
     int id;
     string title;
     int year;
-    Film *next;
-    Relation *relHead;
+    Film* next;
+    Relation* relHead;
 };
 
 struct Actor {
@@ -22,52 +24,46 @@ struct Actor {
     string name;
     int age;
     int performance;
-    Actor *next;
-    Relation *relHead;
+    Actor* next;
+    Relation* relHead;
 };
 
 struct Relation {
-    Film *filmNode;
-    Actor *actorNode;
-    Relation *nextFilm;
-    Relation *nextActor;
+    Film* filmNode;
+    Actor* actorNode;
+    Relation* nextFilm;
+    Relation* nextActor;
 };
-
 Film* createFilm(int id, string title, int year);
 Actor* createActor(int id, string name, int age);
 
-void insertFilm(Film* &head, Film* newFilm);
-void insertActor(Actor* &head, Actor* newActor);
-
-Relation* connectFilmActor(Film* f, Actor* a);
-
-void deleteFilm(Film* &head, Film* target);
-void deleteActor(Actor* &head, Actor* target);
+void insertFilm(Film*& head, Film* newFilm);
+void insertActor(Actor*& head, Actor* newActor);
+void insertFilmByCondition(Film*& head, Film* newFilm);
+void insertActorByCondition(Actor*& head, Actor* newActor);
 
 Film* findFilm(Film* head, int id);
 Actor* findActor(Actor* head, int id);
 
+Relation* connectFilmActor(Film* f, Actor* a);
+bool actorAlreadyLinked(Film* f, Actor* a);
+
+void deleteFilm(Film*& head, Film* target);
+void deleteActor(Actor*& head, Actor* target);
+void deleteFilmByCondition(Film*& head, int yearLimit);
+void deleteActorByCondition(Actor*& head, int ageLimit);
+
+
 void showAllFilms(Film* head);
 void showAllActors(Actor* head);
-
 void showActorsInFilm(Film* f);
 void showFilmsOfActor(Actor* a);
 
-void insertFilmByCondition(Film* &head, Film* newFilm);
-void insertActorByCondition(Actor* &head, Actor* newActor);
-
-void deleteFilmByCondition(Film* &head, int yearLimit);
-void deleteActorByCondition(Actor* &head, int ageLimit);
-
+bool actorHasFilm(Actor* a);
 void searchByCondition(Film* filmHead, Actor* actorHead);
 void computeStatistics(Film* filmHead, Actor* actorHead);
-
-void displayAllParents(Film* head);
-void menuStudiKasus(Film* &filmList, Actor* &actorList);
-
-bool actorHasFilm(Actor* a);
 void assignUnassignedActor(Film* filmHead, Actor* actorHead);
 
-bool actorAlreadyLinked(Film* f, Actor* a);
+void menuStudiKasus(Film*& filmList, Actor*& actorList);
 
 #endif
