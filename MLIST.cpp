307e3d34b65307c42ca65181651c2a3e8172f944
raydaenk <m.rayyadaengraja@gmail.com>
2025-12-20 10:17:27 +0700

    Update MLIST.cpp

--- MLIST.cpp
+++ MLIST.cpp
@@ -1,7 +1,8 @@
 #include "MLIST.h"
 #include <iostream>
-#include <string>
+
 using namespace std;
+
 
 Film* createFilm(int id, string title, int year) {
     Film* f = new Film;
@@ -25,18 +26,45 @@ Actor* createActor(int id, string name, int age) {
 }
 
 
-void insertFilm(Film* &head, Film* newFilm) {
+void insertFilm(Film*& head, Film* newFilm) {
     newFilm->next = head;
     head = newFilm;
 }
 
-void insertActor(Actor* &head, Actor* newActor) {
+void insertActor(Actor*& head, Actor* newActor) {
     newActor->next = head;
     head = newActor;
 }
 
+void insertFilmByCondition(Film*& head, Film* newFilm) {
+    if (!head || newFilm->year < head->year) {
+        newFilm->next = head;
+        head = newFilm;
+        return;
+    }
+    Film* p = head;
+    while (p->next && p->next->year <= newFilm->year)
+        p = p->next;
+    newFilm->next = p->next;
+    p->next = newFilm;
+}
+
+void insertActorByCondition(Actor*& head, Actor* newActor) {
+    if (!head || newActor->age > head->age) {
+        newActor->next = head;
+        head = newActor;
+        return;
+    }
+    Actor* p = head;
+    while (p->next && p->next->age >= newActor->age)
+        p = p->next;
+    newActor->next = p->next;
+    p->next = newActor;
+}
+
+
 Film* findFilm(Film* head, int id) {
-    while (head != nullptr) {
+    while (head) {
         if (head->id == id) return head;
         head = head->next;
     }
@@ -44,15 +72,16 @@ Film* findFilm(Film* head, int id) {
 }
 
 Actor* findActor(Actor* head, int id) {
-    while (head != nullptr) {
+    while (head) {
         if (head->id == id) return head;
         head = head->next;
     }
     return nullptr;
 }
 
-
 Relation* connectFilmActor(Film* f, Actor* a) {
+    if (!f || !a) return nullptr;
+
     Relation* r = new Relation;
     r->filmNode = f;
     r->actorNode = a;
@@ -66,22 +95,30 @@ Relation* connectFilmActor(Film* f, Actor* a) {
     return r;
 }
 
+bool actorAlreadyLinked(Film* f, Actor* a) {
+    if (!f || !a) return false;
+    Relation* r = f->relHead;
+    while (r) {
+        if (r->actorNode == a) return true;
+        r = r->nextFilm;
+    }
+    return false;
+}
 
-void deleteFilm(Film* &head, Film* target) {
+
+void deleteFilm(Film*& head, Film* target) {
+    if (!head || !target) return;
 
     Relation* r = target->relHead;
-    while (r != nullptr) {
-        Relation* next = r->nextFilm;
+    while (r) {
+        Relation* nextRel = r->nextFilm;
 
         Relation* ra = r->actorNode->relHead;
         Relation* prev = nullptr;
-
-        while (ra != nullptr) {
+        while (ra) {
             if (ra == r) {
-                if (prev == nullptr)
-                    r->actorNode->relHead = ra->nextActor;
-                else
-                    prev->nextActor = ra->nextActor;
+                if (!prev) r->actorNode->relHead = ra->nextActor;
+                else prev->nextActor = ra->nextActor;
                 break;
             }
             prev = ra;
@@ -89,35 +126,33 @@ void deleteFilm(Film* &head, Film* target) {
         }
 
         delete r;
-        r = next;
+        r = nextRel;
     }
 
     if (head == target) head = target->next;
     else {
         Film* p = head;
-        while (p->next != nullptr && p->next != target)
+        while (p->next && p->next != target)
             p = p->next;
-        p->next = target->next;
+        if (p->next) p->next = target->next;
     }
+
     delete target;
 }
 
-
-void deleteActor(Actor* &head, Actor* target) {
+void deleteActor(Actor*& head, Actor* target) {
+    if (!head || !target) return;
 
     Relation* r = target->relHead;
-    while (r != nullptr) {
-        Relation* next = r->nextActor;
+    while (r) {
+        Relation* nextRel = r->nextActor;
 
         Relation* rf = r->filmNode->relHead;
         Relation* prev = nullptr;
-
-        while (rf != nullptr) {
+        while (rf) {
             if (rf == r) {
-                if (prev == nullptr)
-                    r->filmNode->relHead = rf->nextFilm;
-                else
-                    prev->nextFilm = rf->nextFilm;
+                if (!prev) r->filmNode->relHead = rf->nextFilm;
+                else prev->nextFilm = rf->nextFilm;
                 break;
             }
             prev = rf;
@@ -125,21 +160,43 @@ void deleteActor(Actor* &head, Actor* target) {
         }
 
         delete r;
-        r = next;
+        r = nextRel;
     }
 
     if (head == target) head = target->next;
     else {
         Actor* p = head;
-        while (p->next != nullptr && p->next != target)
+        while (p->next && p->next != target)
             p = p->next;
-        p->next = target->next;
+        if (p->next) p->next = target->next;
     }
+
     delete target;
 }
 
+void deleteFilmByCondition(Film*& head, int yearLimit) {
+    Film* curr = head;
+    while (curr) {
+        Film* next = curr->next;
+        if (curr->year < yearLimit)
+            deleteFilm(head, curr);
+        curr = next;
+    }
+}
+
+void deleteActorByCondition(Actor*& head, int limit) {
+    Actor* curr = head;
+    while (curr) {
+        Actor* next = curr->next;
+        if (curr->performance < limit)
+            deleteActor(head, curr);
+        curr = next;
+    }
+}
+
+
 void showAllFilms(Film* head) {
-    while (head != nullptr) {
+    while (head) {
         cout << head->id << " - " << head->title
              << " (" << head->year << ")\n";
         head = head->next;
@@ -147,7 +204,7 @@ void showAllFilms(Film* head) {
 }
 
 void showAllActors(Actor* head) {
-    while (head != nullptr) {
+    while (head) {
         cout << head->id << " - " << head->name
              << " | Age: " << head->age
              << " | Perf: " << head->performance << "\n";
@@ -156,143 +213,73 @@ void showAllActors(Actor* head) {
 }
 
 void showActorsInFilm(Film* f) {
-    cout << "Aktor di film: " << f->title << "\n";
+    if (!f) return;
     Relation* r = f->relHead;
-    while (r != nullptr) {
-        cout << "- " << r->actorNode->name << "\n";
+    cout << "Aktor di film: " << f->title << endl;
+    while (r) {
+        cout << "- " << r->actorNode->name << endl;
         r = r->nextFilm;
     }
 }
 
 void showFilmsOfActor(Actor* a) {
-    cout << "Film dari aktor: " << a->name << "\n";
+    if (!a) return;
     Relation* r = a->relHead;
-    while (r != nullptr) {
-        cout << "- " << r->filmNode->title << "\n";
+    cout << "Film dari aktor: " << a->name << endl;
+    while (r) {
+        cout << "- " << r->filmNode->title << endl;
         r = r->nextActor;
     }
 }
 
-
-
-void insertFilmByCondition(Film* &head, Film* newFilm) {
-    if (head == nullptr || newFilm->year < head->year) {
-        newFilm->next = head;
-        head = newFilm;
-        return;
-    }
-    Film* p = head;
-    while (p->next != nullptr && p->next->year <= newFilm->year)
-        p = p->next;
-
-    newFilm->next = p->next;
-    p->next = newFilm;
+bool actorHasFilm(Actor* a) {
+    return a && a->relHead != nullptr;
 }
-
-void insertActorByCondition(Actor* &head, Actor* newActor) {
-    if (head == nullptr || newActor->age > head->age) {
-        newActor->next = head;
-        head = newActor;
-        return;
-    }
-    Actor* p = head;
-    while (p->next != nullptr && p->next->age >= newActor->age)
-        p = p->next;
-
-    newActor->next = p->next;
-    p->next = newActor;
-}
-
-void deleteFilmByCondition(Film* &head, int yearLimit) {
-    Film* curr = head;
-    while (curr != nullptr) {
-        Film* next = curr->next;
-        if (curr->year < yearLimit)
-            deleteFilm(head, curr);
-        curr = next;
-    }
-}
-
-void deleteActorByCondition(Actor* &head, int ageLimit) {
-    Actor* curr = head;
-    while (curr != nullptr) {
-        Actor* next = curr->next;
-        if (curr->age > ageLimit)
-            deleteActor(head, curr);
-        curr = next;
-    }
-}
-
 
 void searchByCondition(Film* filmHead, Actor* actorHead) {
     int year;
     cout << "Cari film dengan tahun > ";
     cin >> year;
-
-    while (filmHead != nullptr) {
+    while (filmHead) {
         if (filmHead->year > year)
-            cout << "- " << filmHead->title << "\n";
+            cout << "- " << filmHead->title << endl;
         filmHead = filmHead->next;
     }
 }
 
 void computeStatistics(Film* filmHead, Actor* actorHead) {
-    int filmCount = 0;
-    int actorCount = 0;
-
-    while (filmHead != nullptr) {
-        filmCount++;
-        filmHead = filmHead->next;
-    }
-
-    while (actorHead != nullptr) {
-        actorCount++;
-        actorHead = actorHead->next;
-    }
-
-    cout << "Total Film = " << filmCount << endl;
+    int filmCount = 0, actorCount = 0;
+    while (filmHead) { filmCount++; filmHead = filmHead->next; }
+    while (actorHead) { actorCount++; actorHead = actorHead->next; }
+    cout << "Total Film  = " << filmCount << endl;
     cout << "Total Aktor = " << actorCount << endl;
 }
 
-
-void displayAllParents(Film* head) {
-    showAllFilms(head);
+void assignUnassignedActor(Film* filmHead, Actor* actorHead) {
+    Actor* a = actorHead;
+    cout << "Aktor tanpa film:\n";
+    while (a) {
+        if (!actorHasFilm(a))
+            cout << a->id << " - " << a->name << endl;
+        a = a->next;
     }
 
-bool actorHasFilm(Actor* a) {
-    return (a->relHead != nullptr);
-}
+    int aid, fid;
+    cout << "ID Aktor: "; cin >> aid;
+    cout << "ID Film: "; cin >> fid;
 
-void showUnassignedActors(Actor* head) {
-    cout << "\n=== Aktor yang Belum Bermain di Film Manapun ===\n";
-    bool found = false;
+    Actor* A = findActor(actorHead, aid);
+    Film* F = findFilm(filmHead, fid);
 
-    Actor* p = head;
-    while (p != nullptr) {
-        if (!actorHasFilm(p)) {
-            cout << "ID: " << p->id
-                 << " | Nama: " << p->name
-                 << " | Usia: " << p->age << endl;
-            found = true;
-        }
-        p = p->next;
+    if (A && F && !actorAlreadyLinked(F, A)) {
+        connectFilmActor(F, A);
+        cout << "Berhasil di-assign.\n";
     }
-
-    if (!found) cout << "(Semua aktor sudah memiliki film)\n";
-}
-
-bool actorAlreadyLinked(Film* f, Actor* a) {
-    Relation* r = f->relHead;
-    while (r != nullptr) {
-        if (r->actorNode == a) return true;
-        r = r->nextFilm;
-    }
-    return false;
 }
 
 
-void menuStudiKasus(Film* &filmList, Actor* &actorList) {
-    cout << "\n========== STUDI KASUS ==========\n";
+void menuStudiKasus(Film*& filmList, Actor*& actorList) {
+    cout << "\n=== STUDI KASUS ===\n";
 
     insertFilmByCondition(filmList, createFilm(501, "Avatar", 2009));
     insertFilmByCondition(filmList, createFilm(502, "Avengers", 2012));
@@ -306,33 +293,6 @@ void menuStudiKasus(Film* &filmList, Actor* &actorList) {
     connectFilmActor(findFilm(filmList, 502), findActor(actorList, 602));
     connectFilmActor(findFilm(filmList, 503), findActor(actorList, 601));
 
-    cout << "\n>>> Setelah insert otomatis\n";
     showAllFilms(filmList);
-    cout << endl;
     showAllActors(actorList);
-
-    cout << "\n>>> Searching film tahun > 2000\n";
-    Film* p = filmList;
-    while (p != nullptr) {
-        if (p->year > 2000)
-            cout << "- " << p->title << endl;
-        p = p->next;
-    }
-
-    cout << "\n>>> Statistik\n";
-    computeStatistics(filmList, actorList);
-
-    cout << "\n>>> Hapus film tahun < 2005\n";
-    deleteFilmByCondition(filmList, 2005);
-
-    cout << "\n>>> Hapus aktor usia > 53\n";
-    deleteActorByCondition(actorList, 53);
-
-    cout << "\n>>> Data Film (Final):\n";
-    showAllFilms(filmList);
-
-    cout << "\n>>> Data Aktor (Final):\n";
-    showAllActors(actorList);
-
-    cout << "\n========== SELESAI STUDI KASUS ==========\n";
 }
